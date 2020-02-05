562898eb44456baeb30a0e80bc56e6727810f36f
G00359991 <G00359991@gmit.ie>
2020-02-05 16:05:37 +0000

    Added js and php

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <body>
             <h1>My Website</h1>
             <p id=demo>Hello World!</p>
+            <?php echo "<p>Hello world PHP</p>" ; ?>
             <script type="text/javascript" src="myscripts.js"></script>
             <button onclick="myFunction();">Click Me to turn text blue</button>
             
