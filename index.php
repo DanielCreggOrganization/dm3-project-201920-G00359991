a45c294ee42ffacec5d7be83fc8afc5ea9513a18
G00359991 <G00359991@gmit.ie>
2020-02-17 15:47:59 +0000

    added some stuff

--- index.php
+++ index.php
@@ -4,6 +4,14 @@
         
     </head>
     <body>
+
+    <div class="topnav">
+      <a class="active" href="#home">Home</a>
+      <a href="#about">About</a>
+      <a href="#gamesonsale">Games on sale</a>
+      <a href="#checkout">Checkout</a>
+      <a href="#contact">Contact</a>
+    </div>
             <h1>My Website</h1>
             <p id=demo>Hello World!</p>
             <?php echo "<p>Hello world PHP</p>" ; ?>
