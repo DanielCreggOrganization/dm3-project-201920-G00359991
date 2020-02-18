bebb355426dce34fa50440b6ac2caf1db04092f5
G00359991 <G00359991@gmit.ie>
2020-02-18 09:23:04 +0000

    Added Bootstrap and a Footer

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <html>
     <head>
         <link rel="stylesheet" href="styles.css">
-        
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<!-- Bootstrap CSS -->
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+		<link rel="stylesheet" href="css/style1.css" media="screen" type="text/css" />
+			<link rel="stylesheet" type+"text/css" href="homepage.css">
+		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
+		<script type="text/javascript" src="js/jquery.shop.js"></script>
     </head>
     <body>
 
@@ -18,5 +25,9 @@
             <script type="text/javascript" src="myscripts.js"></script>
             <button onclick="myFunction();">Click Me to turn text blue</button>
             
+            <footer>
+            <p>Posted by: Admin</p>
+            <p>Contact information: <a href="mailto:retrogames@gmail.com">retrogames@gmail.com</a>.</p>
+            </footer>
     </body>
 </html>
