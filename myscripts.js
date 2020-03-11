b7361f05c9cc14c68d9c72b5ab1f745179e4673f
G00359991 <G00359991@gmit.ie>
2020-03-11 13:57:52 +0000

    deleted unnecessary pages and added js.

--- myscripts.js
+++ myscripts.js
@@ -1,3 +1,15 @@
 function myFunction(){
     document.getElementById("demo").style.color = "blue";
 }
+// Activate Carousel
+$("#demo").carousel();
+
+// Enable Carousel Indicators
+$(".carousel-item").click(function(){
+  $("#demo").carousel(1);
+});
+
+// Enable Carousel Controls
+$(".left").click(function(){
+  $("#demo").carousel("prev");
+});
