fef6c9bfe8955eae0e188f44afaa7a4da0492eae
G00359991 <G00359991@gmit.ie>
2020-04-24 14:18:23 +0000

    added in vendor files and a calender.

--- about.php
+++ about.php
@@ -1,6 +1,7 @@
 <html>
     <head>
         <link rel="stylesheet" href="styles.css">
+        <link href="/mysql/css/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -46,3 +47,7 @@
   <img src="/mysql/img/gmit.jpg" alt="GMIT" class="gmit">
   <p style="text-align:centre;">We are Retro Games, a company based out of Galway which specialises in selling old-school playstation games from the 1990's and early 2000's. We chose Galway as our base of operations to sell our wares because we knew the city well, and the people around here were very nice and welcoming to us and our business. RetroGames was started up by it's founder, Mr. Billy O'Reilly, who is an avid gamer himself, but one who is more into old-school titles than more modern ones. Games like classic Tomb Raider or Crash Bandicoot, which are currently sold here by us rather than the flashier games of the current decade like God Of War or Grand Theft Auto V.</p>
 </div>
+
+<iframe src="https://calendar.google.com/calendar/embed?src=en.irish%23holiday%40group.v.calendar.google.com&ctz=Europe%2FDublin" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+</body>
+</html>
